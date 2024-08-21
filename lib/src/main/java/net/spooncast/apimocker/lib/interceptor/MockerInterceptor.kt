10707f47d04c2b6e204da19296e59d25c300ec54
Seop-Seo <96706890+Seop-Seo@users.noreply.github.com>
2024-08-21 11:04:28 +0900

    AOSTEAM-2136 [API 모킹][lib] MockerInterceptor 개발 (#3)
    * AOSTEAM-2136 [API 모킹][lib] MockerInterceptor 개발
    
    * AOSTEAM-2136 [API 모킹][lib] MockerInterceptor 개발

---
+++ lib/src/main/java/net/spooncast/apimocker/lib/interceptor/MockerInterceptor.kt
@@ -0,0 +1,41 @@
+package net.spooncast.apimocker.lib.interceptor
+
+import net.spooncast.apimocker.lib.repo.MockerRepo
+import okhttp3.Interceptor
+import okhttp3.Protocol
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import javax.inject.Inject
+
+class MockerInterceptor @Inject constructor(
+    private val mockerRepo: MockerRepo
+): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val mocked = mockerRepo.getMock(request)
+
+        if (mocked != null) {
+            return Response.Builder()
+                .protocol(Protocol.HTTP_2)
+                .request(request)
+                .code(mocked.code)
+                .message(MOCKER_MESSAGE)
+                .body(MOCKER_MESSAGE.toResponseBody(null))
+                .build()
+        }
+
+        val response = chain.proceed(request)
+
+        // 성공한 요청에 대해서만 caching을 수행한다.
+        if (response.isSuccessful) {
+            mockerRepo.cache(request, response)
+        }
+
+        return response
+    }
+
+    companion object {
+        const val MOCKER_MESSAGE = "Spoon api mocker enabled"
+    }
+}
