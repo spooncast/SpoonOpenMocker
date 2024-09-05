9271e1ab0413d13566af129b557d04cd45a21b58
Seop-Seo <96706890+Seop-Seo@users.noreply.github.com>
2024-09-05 14:56:33 +0900

    Added single umock flow #28 (#29)

---
+++ lib/src/main/java/net/spooncast/openmocker/lib/ui/common/TwoButtons.kt
@@ -0,0 +1,37 @@
+package net.spooncast.openmocker.lib.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import net.spooncast.openmocker.lib.R
+
+@Composable
+fun TwoButtons(
+    modifier: Modifier = Modifier,
+    onClickCancel: () -> Unit,
+    onClickOk: () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Button(
+            onClick = onClickCancel,
+            modifier = Modifier.weight(1F, true)
+        ) {
+            Text(text = stringResource(id = R.string.common_cancel))
+        }
+        Button(
+            onClick = onClickOk,
+            modifier = Modifier.weight(1F, true)
+        ) {
+            Text(text = stringResource(id = R.string.common_ok))
+        }
+    }
+}
---
+++ lib/src/main/java/net/spooncast/openmocker/lib/ui/list/dialog/UnMockDialog.kt
@@ -0,0 +1,59 @@
+package net.spooncast.openmocker.lib.ui.list.dialog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import net.spooncast.openmocker.lib.R
+import net.spooncast.openmocker.lib.ui.common.PreviewWithCondition
+import net.spooncast.openmocker.lib.ui.common.TwoButtons
+import net.spooncast.openmocker.lib.ui.common.VerticalSpacer
+
+@Composable
+fun UnMockDialog(
+    onDismiss: () -> Unit,
+    onClickOk: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(10.dp))
+                .padding(20.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.open_mocker),
+                style = MaterialTheme.typography.titleMedium
+            )
+            VerticalSpacer(size = 10.dp)
+
+            Text(text = stringResource(id = R.string.release_mocking))
+
+            VerticalSpacer(size = 20.dp)
+
+            TwoButtons(
+                onClickCancel = onDismiss,
+                onClickOk = onClickOk
+            )
+        }
+    }
+}
+
+@PreviewWithCondition
+@Composable
+private fun PreviewUnMockDialog() {
+    MaterialTheme {
+        UnMockDialog(
+            onDismiss = {},
+            onClickOk = {}
+        )
+    }
+}
