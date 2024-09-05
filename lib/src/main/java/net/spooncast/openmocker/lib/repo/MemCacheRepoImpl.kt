e00841524755cbb666074066d2cc5e66fbebf609
Seop (SeongHyuk Seo) <seop@spoonradio.co>
2024-09-05 11:27:03 +0900

    Replace logic which change jsonString to pretty from loop to gson #22

--- lib/src/main/java/net/spooncast/openmocker/lib/repo/MemCacheRepoImpl.kt
+++ lib/src/main/java/net/spooncast/openmocker/lib/repo/MemCacheRepoImpl.kt
@@ -1,7 +1,8 @@
 package net.spooncast.openmocker.lib.repo
 
 import androidx.compose.runtime.mutableStateMapOf
-import net.spooncast.openmocker.lib.ext.pretty
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
 import net.spooncast.openmocker.lib.model.CachedKey
 import net.spooncast.openmocker.lib.model.CachedResponse
 import net.spooncast.openmocker.lib.model.CachedValue
@@ -11,14 +12,19 @@ import kotlin.concurrent.Volatile
 
 class MemCacheRepoImpl private constructor(): CacheRepo {
 
+    private val gson by lazy { GsonBuilder().setPrettyPrinting().create() }
+
     private val _cachedMap = mutableStateMapOf<CachedKey, CachedValue>()
     override val cachedMap: Map<CachedKey, CachedValue> get() = _cachedMap
 
     override fun cache(request: Request, response: Response) {
         val key = CachedKey(request.method, request.url.encodedPath)
 
-        val body = runCatching { response.peekBody(MAX_BYTE_COUNT).string().pretty() }
-            .getOrDefault("")
+        val body = runCatching {
+            val bodyString = response.peekBody(MAX_BYTE_COUNT).string()
+            val jsonElement = JsonParser.parseString(bodyString)
+            gson.toJson(jsonElement)
+        }.getOrDefault("")
         val cachedResponse = CachedResponse(response.code, body)
 
         _cachedMap[key] = CachedValue(response = cachedResponse)
