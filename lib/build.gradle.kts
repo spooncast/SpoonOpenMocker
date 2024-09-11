bca9c6c0df04175b0eccd33c7080bd64074f8d25
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2024-09-11 15:59:25 +0900

    release 0.0.7

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -81,7 +81,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "net.spooncast"
             artifactId = "openmocker"
-            version = "0.0.6"
+            version = "0.0.7"
 
             afterEvaluate {
                 from(components["release"])
