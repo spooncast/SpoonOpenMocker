5ea03de9627b7ff723b43afc9dc62be4dbb231dc
Seop (SeongHyuk Seo) <seop@spoonradio.co>
2024-09-11 17:19:58 +0900

    release 0.0.8

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -81,7 +81,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "net.spooncast"
             artifactId = "openmocker"
-            version = "0.0.7"
+            version = "0.0.8"
 
             afterEvaluate {
                 from(components["release"])
